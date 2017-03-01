7f874779e8b02901431a20fe2f7f7e2b423005a7
Robin <robin.tissot@free.fr>
2017-03-01 22:55:53 +0100

    Amélioration mineure de la fonction addExercice

--- src/bdd/Sql.java
+++ src/bdd/Sql.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Time;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by Hamor.
@@ -16,26 +18,31 @@ public abstract class Sql{
         dbConnexion = c;
     }
 
-    public static boolean addExercice(int numeroExercice, Time dureeExercice, int pointsExercice, String libelleExercice, String fichierExercicePath, String tags) throws SQLException {
-        String insertExercice = "INSERT INTO EXERCICE"
-                + "(numeroExercice, dureeExercice, pointsExercice, libelleExercice, fichierExercice) VALUES"
-                + "(?,?,?,?,?)";
-        PreparedStatement preparedStatement = dbConnexion.prepareStatement(insertExercice);
-        preparedStatement.setInt(1, numeroExercice);
-        preparedStatement.setTime(2, dureeExercice);
-        preparedStatement.setInt(3, pointsExercice);
-        preparedStatement.setString(4, libelleExercice);
-        preparedStatement.setString(5, tags);
-        if (preparedStatement.executeUpdate() == 0){
-            return false;
+    public static boolean addExercice(int numeroExercice, Time dureeExercice, int pointsExercice, String libelleExercice, String fichierExercicePath, String tags) {
+        try {
+            String insertExercice = "INSERT INTO EXERCICE"
+                    + "(numeroExercice, dureeExercice, pointsExercice, libelleExercice, fichierExercice) VALUES"
+                    + "(?,?,?,?,?)";
+            PreparedStatement preparedStatement = dbConnexion.prepareStatement(insertExercice);
+            preparedStatement.setInt(1, numeroExercice);
+            preparedStatement.setTime(2, dureeExercice);
+            preparedStatement.setInt(3, pointsExercice);
+            preparedStatement.setString(4, libelleExercice);
+            preparedStatement.setString(5, tags);
+            if (preparedStatement.executeUpdate() == 0){
+                return false;
+            }
+            int id = ((Number) preparedStatement.executeQuery("Select last_inster_rowid();")).intValue();
+            System.out.println(id);
+            Database.getINSTANCE().addExercice(id, numeroExercice, dureeExercice, pointsExercice, libelleExercice,
+                    fichierExercicePath, tags);
+            
+            //numeroExercice, dureeExercice, pointsExercice, libelleExercice, fichierExercicePath, tags);
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Sql.class.getName()).log(Level.SEVERE, null, ex);
         }
-        int id = ((Number) preparedStatement.executeQuery("Select last_inster_rowid();")).intValue();
-        System.out.println(id);
-        Database.getINSTANCE().addExercice(id, numeroExercice, dureeExercice, pointsExercice, libelleExercice,
-                fichierExercicePath, tags);
-
-        //numeroExercice, dureeExercice, pointsExercice, libelleExercice, fichierExercicePath, tags);
-    return true;
+        return false;
     }
 
     public static boolean addChapitre(int numeroChapitre, boolean presentiel, String libelle){
